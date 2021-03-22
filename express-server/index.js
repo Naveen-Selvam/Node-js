d3e4d149e74380a5cfc1170f61cb358df2728a5b
naveens <joinnaveens@gmail.com>
2021-03-22 15:13:22 +0530

    updated

--- express-server/index.js
+++ express-server/index.js
@@ -13,8 +13,56 @@ mongoose
     console.log('errmessage', err);
   });
 
+//schema
+const Schema = mongoose.Schema;
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  completed: {
+    type: Boolean,
+  },
+  dueDate: {
+    type: Date,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+//model
+const task = mongoose.model('task', taskSchema);
+
 app.get('/', (req, res) => {
   res.send('welcome');
+});
+
+app.get('/api/task', (req, res) => {
+  task
+    .find()
+    .then((tasks) => {
+      res.json(tasks);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+app.post('/api/task', (req, res) => {
+  const body = req.body;
+  const Task = new task(body);
+  Task.save()
+    .then((task) => {
+      res.json(task);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 app.listen(port, () => {
